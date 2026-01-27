55ac765f47301e4078fa7f0b718ffd453ec88dc3
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-27 11:27:10 +0900

    モデルを有効化

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 自分にアプリケーション
+    'learning_logs'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
