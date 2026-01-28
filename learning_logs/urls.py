4dfc1f3c2892fdacf78fc31d28c40f7ce2aead7b
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-28 13:05:54 +0900

    urlの対応付けとreadmeを追加

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+"""learning_logsのURLパターンの定義"""
+
+
+from django.urls import path
+
+from learning_logs import views
+
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # ホームページ
+    path('',views.index,name='index'),
+]
