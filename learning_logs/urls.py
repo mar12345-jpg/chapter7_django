8cf85fc5f1faf754942a4e4a2cade3ad8d25bd50
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-28 15:15:25 +0900

    トピック一覧ページ作成

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,15 +1,17 @@
 """learning_logsのURLパターンの定義"""
 
-
+# アプリ内のパス指定
 from django.urls import path
 
 from learning_logs import views
 
-
+# アプリ名を記述
 app_name = 'learning_logs'
 urlpatterns = [
     # ホームページ
     # パス文字れる => 関数名,パスに別名を付ける
-    # index = ''
+    # learning_logs:index = ''
+    # {% url "learning_logs:index" %}
     path('',views.index,name='index'),
+    path('topics/',views.topics,name='topics'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from learning_logs.models import Topic
+
 # Create your views here.
 
 def index(request):
     """学習ノートのホームページ"""
     # tenplates/アプリ名フォルダ/を探す
     return render(request,'learning_logs/index.html')
+
+def topics(request):
+    """すべてのトピックを表示する"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+    return render(request,'learning_logs/topics.html',context)
