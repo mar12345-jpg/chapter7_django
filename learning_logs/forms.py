7a2dfa1e6d401cb706eb6f178e17035477ec7141
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-29 11:49:57 +0900

    新規トピック追加ページのurlパターン追加

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from .models import Topic
 
 class TopicForm(forms.ModelForm):
-    class Meta:
-        model = Topic # 対応するモデル(テーブル)名
+    class Meta: # 概要情報
+        # フォーム化対象のモデル(テーブル)名。ここにデータが保存される
+        model = Topic # 対応するモデル
         fields = ['text'] # 入力対象
         labels = {'text':''} # HTMLでのラベル表示
