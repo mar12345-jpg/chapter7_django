812db5af4389492467181a4306d6a7c3a748544d
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-29 11:41:04 +0900

    タイプミス修正

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Topic
 
-class ToppicForm(forms.ModelForm):
+class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic # 対応するモデル(テーブル)名
         fields = ['text'] # 入力対象
